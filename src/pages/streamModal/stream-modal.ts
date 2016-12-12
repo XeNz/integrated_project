e796b643500ee0fd715484402edc85e8784e886e
BartDeCaluwe <de.caluwe.bart@gmail.com>
2016-12-12 18:21:29 +0100

    Back button added to modal stream

--- src/pages/streamModal/stream-modal.ts
+++ src/pages/streamModal/stream-modal.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ViewController } from 'ionic-angular';
 
 @Component({
     templateUrl: 'stream-modal.html'
 })
 export class StreamPage {
-    constructor(public navCtrl: NavController) { }
+    constructor(public navCtrl: NavController, private viewCtrl: ViewController) { }
 
-    ionViewDidLoad() {
-        console.log('Hello stream Page');
+    dismiss(){
+        this.viewCtrl.dismiss();
     }
 }
